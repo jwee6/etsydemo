cd95889e8ba970e557d12e861d7f8fb72a1c727c
Jason Wee <jasonwee_1369@hotmail.com>
2015-10-25 18:38:21 -0500

    modified alert display

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #Required for devise. Change localhost to application host
+  #Required for devise. Change localhost to application host eg www.etsydemo.com
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
