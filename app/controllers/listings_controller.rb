b42cc2372a06243cbfbbd2ad0502ef6115947ea7
Jason Wee <jasonwee_1369@hotmail.com>
2015-10-26 20:43:22 -0500

    Add Seller Page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,6 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :seller]
   before_filter :check_user, only: [:edit, :update, :destroy]
 
   # GET /listings
@@ -63,6 +63,10 @@ class ListingsController < ApplicationController
       format.html { redirect_to listings_url, notice: 'Listing was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def seller
+    @listings = Listing::where(user: current_user).order("created_at DESC")
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'pages/contact'
 
   get 'pages/about'
+  get 'seller' => 'listings#seller' #localhost/seller categorize seller under listings pages
 
   root 'listings#index'
 
